bd4237f46164e3c6079b15b25699046177c14749
jittagorn pitakmetagoon <jittagornp@gmail.com>
2015-06-03 13:45:59 +0700

    initial commit

---
+++ src/com/blogspot/na5cent/learning/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blogspot.na5cent.learning;
+
+/**
+ *
+ * @author jittagornp
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Park.");
+        System.out.println(4 + 5);
+        System.out.println(2 * 3);
+    }
+    
+}
